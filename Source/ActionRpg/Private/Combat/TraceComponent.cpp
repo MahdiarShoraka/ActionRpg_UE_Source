66b9a85c7e4dd4ce1ea288fdaf91aa04fd7a9ce8
Mahdiar Shoraka <mahdiarshoraka@gmail.com>
2025-08-28 17:29:55 -0400

    Added basic Tracing for combat

--- Source/ActionRpg/Private/Combat/TraceComponent.cpp
+++ Source/ActionRpg/Private/Combat/TraceComponent.cpp
@@ -2,33 +2,66 @@
 
 
 #include "Combat/TraceComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
 
-// Sets default values for this component's properties
 UTraceComponent::UTraceComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
-// Called when the game starts
 void UTraceComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
 	
+	SkeletalComp = GetOwner()->FindComponentByClass<USkeletalMeshComponent>();
 }
 
 
-// Called every frame
+
 void UTraceComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	FVector StartSocketLocation = SkeletalComp->GetSocketLocation(Start);
+	FVector EndSocketLocation = SkeletalComp->GetSocketLocation(End);
+	FQuat ShapeRotation = SkeletalComp->GetSocketQuaternion(Rotation);
+
+	TArray<FHitResult> OutResults;
+	double WeaponDistance = FVector::Distance(StartSocketLocation, EndSocketLocation);
+
+	FVector BoxHalfExtents = FVector(BoxCollisionLength, BoxCollisionLength, WeaponDistance);
+	BoxHalfExtents /= 2;
+	FCollisionShape Box = FCollisionShape::MakeBox(BoxHalfExtents);
+
+	FCollisionQueryParams IgnoreParams(FName(TEXT("Ignore Params")), false, GetOwner());
+	
+	bool bHasFoundTargets = GetWorld()->SweepMultiByChannel(
+		OutResults,
+		StartSocketLocation,
+		EndSocketLocation,
+		ShapeRotation,
+		ECC_GameTraceChannel1,
+		Box,
+		IgnoreParams);
+
+	if (bDebugMode)
+	{
+		FVector CenterPoint = UKismetMathLibrary::VLerp(
+			StartSocketLocation,
+			EndSocketLocation,
+			0.5f);	// Vector Linear Interpolation
+
+		UKismetSystemLibrary::DrawDebugBox(
+			GetWorld(),
+			CenterPoint, 
+			Box.GetExtent(),
+			bHasFoundTargets ? FLinearColor::Green : FLinearColor::Red,
+			ShapeRotation.Rotator(),
+			1.0f,
+			2.0f);
+	}
 }
 
--- Source/ActionRpg/Public/Combat/TraceComponent.h
+++ Source/ActionRpg/Public/Combat/TraceComponent.h
@@ -6,6 +6,7 @@
 #include "Components/ActorComponent.h"
 #include "TraceComponent.generated.h"
 
+class USkeletalMeshComponent;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ACTIONRPG_API UTraceComponent : public UActorComponent
@@ -13,16 +14,28 @@ class ACTIONRPG_API UTraceComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UTraceComponent();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	USkeletalMeshComponent* SkeletalComp;
+
+	UPROPERTY(EditAnywhere)
+	FName Start;
+
+	UPROPERTY(EditAnywhere)
+	FName End;
+
+	UPROPERTY(EditAnywhere)
+	FName Rotation;
+
+	UPROPERTY(EditAnywhere)
+	double BoxCollisionLength = 30.0;
+
+	UPROPERTY(EditAnywhere)
+	bool bDebugMode = false;
 };
